165ea255af44b6f533a49bc8beae8250845a95a9
soKaradag <so.karadag@gmail.com>
2026-02-16 02:53:51 +0300

    Solution9

---
+++ Solution9.java
@@ -0,0 +1,45 @@
+class Solution9 {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(10));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+
+    public static boolean isPalindromeWithString(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        String s = String.valueOf(x);
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
